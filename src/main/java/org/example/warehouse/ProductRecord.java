8769108db615570dd66c884d85d7ab0cca5c63e0
august <august.rydnell@gmail.com>
2023-09-25 11:43:26 +0200

    added arraylist for the productRecords, filled in some fields in ProductRecord class, created method for creating a new product in warehouse class

--- src/main/java/org/example/warehouse/ProductRecord.java
+++ src/main/java/org/example/warehouse/ProductRecord.java
@@ -1,4 +1,15 @@
 package org.example.warehouse;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public class ProductRecord {
+
+    UUID productUUID;
+    String productName;
+    BigDecimal productPrice;
+    // todo add category object
+
+
+
 }
--- src/main/java/org/example/warehouse/Warehouse.java
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -1,4 +1,19 @@
 package org.example.warehouse;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Warehouse {
+    /**list that contains all the products in the store*/
+    ArrayList<ProductRecord> listOfProducts = new ArrayList<>();
+
+
+    /**
+     * Creates a product of the type ProductRecord.
+     * */
+    public void addProduct(UUID id, String name, Object category, BigDecimal price) {
+    }
+
+
 }
